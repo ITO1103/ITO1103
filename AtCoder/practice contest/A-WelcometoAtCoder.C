f7048ec11ba155b66131163da5dd62a3101f188e
ITO110 <t22j209ai@ous.jp>
2023-05-21 00:18:05 +0900

    Start AtCoder

---
+++ AtCoder/practice contest/A-WelcometoAtCoder.C
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    char s[101];
+    scanf("%d",&a);
+    scanf("%d %d",&b,&c);
+    scanf("%s",s);
+    printf("%d %s\n",a+b+c,s);
+    return 0;
+}
