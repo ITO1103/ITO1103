3eb8026f5a09b6c9903e2d1232d36d3aadf7ae8f
ITO110 <t22j209ai@ous.jp>
2023-07-05 21:32:18 +0900

    update

---
+++ AtCoder/APG4b[C++]/J.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int x, a, b;
+  cin >> x >> a >> b;
+
+  // 1.の出力
+  cout << x << endl;
+
+  // ここにプログラムを追記
+  x++;
+  cout << x <<endl;
+
+  x += a b;
+  cout << x << endl;
+
+  x *= x x;
+  cout << x << endl;
+
+  x--;
+  cout << x << endl;
+
+}
