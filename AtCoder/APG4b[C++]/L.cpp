b40bea4e59d70715710a23931ea1fa4e0323b9e4
ITO110 <t22j209ai@ous.jp>
2023-07-18 23:36:41 +0900

    update

--- AtCoder/APG4b[C++]/L.cpp
+++ AtCoder/APG4b[C++]/L.cpp
@@ -1,5 +1,29 @@
 #include <bits/stdc++.h>
+using namespace std;
 
-int main(){
-    
+int main() {
+  int N, A, i;
+  cin >> N >> A;
+
+  // ここにプログラムを追記
+  for(i=0;i<N;i++){
+    int x;
+    string Kigo;
+
+    cin >> Kigo >> x;
+
+    if(Kigo=="+"){
+        A +=x;
+    }else if(Kigo=="-"){
+        A -=x;
+    }else if(Kigo=="*"){
+        A *=x;
+    }else if(Kigo=="/" && x !=0){
+        A /=x;
+    }else{
+        cout << "error" << endl;
+        break;
+    }
+    cout << i+1 << ":" << A << endl;
+  }
 }
