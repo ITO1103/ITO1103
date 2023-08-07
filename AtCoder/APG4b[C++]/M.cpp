9aad1ed87452a92e71419da2c799464dda7f8ffd
ITO110 <t22j209ai@ous.jp>
2023-08-07 15:28:23 +0900

    update

---
+++ AtCoder/APG4b[C++]/M.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(void){
+    // 自分の得意な言語で
+    // スキルチェックの基本となる、標準入力で値を取得し、
+    // 出力するコードを書いてみよう！
+    string str;
+    getline(cin, str);
+    cout << str << endl;
+    return 0;
+}
