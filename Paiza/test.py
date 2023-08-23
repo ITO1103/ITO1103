fe9a43671c425648a2d151cd7eec0acb606ed8d4
ITO110 <t22j209ai@ous.jp>
2023-08-23 23:23:58 +0900

    update

--- Paiza/test.py
+++ Paiza/test.py
@@ -1,2 +1,21 @@
-characters = {("girl", "cute"): "kirishima", ("boy", "cool"): "kaga"}
-print(characters)
+character = {"name": "kirishima", "age": 18, "pc": "margaret"}
+print(character.keys())
+
+print("***********")
+
+character = {"name": "kirishima", "age": 18, "pc": "margaret"}
+print(character.items())
+
+print("############")
+
+character = {"name": "kirishima", "age": 18, "pc": "margaret"}
+k = input()
+print(k in character)
+
+print("||||||||||||")
+
+n = int(input())
+d = {}
+for i in range(n):
+    d[i] = i * 3
+print(d)
