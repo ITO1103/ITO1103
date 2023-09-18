5a437d3480264be5b4cd2d203745774c35194ccb
ITO110 <t22j209ai@ous.jp>
2023-09-18 23:18:36 +0900

    update

--- AtCoder/APG4b[C++]/O.cpp
+++ AtCoder/APG4b[C++]/O.cpp
@@ -5,6 +5,10 @@ int main() {
     int A, B, C;
     cin >> A >> B >> C;
 
-    
+    //入力ABCをvectorに格納
+    vector<int> vec = {A, B, C};
+    sort(vec.begin(), vec.end());
 
+    //出力
+    cout << vec.at(2) - vec.at(0) << endl;
 }
