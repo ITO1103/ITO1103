0b80d8cada5ca90b7910b650afe1d882b0f76f4f
ITO110 <t22j209ai@ous.jp>
2023-09-20 23:30:55 +0900

    change name and update

--- AtCoder/APG4b[C++]/test.cpp
+++ AtCoder/APG4b[C++]/test.cpp
@@ -1,11 +1,39 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-    vector<int> vec = {2, 5, 2, 1};
-    sort(vec.begin(), vec.end()); // {1, 2, 2, 5}
+int sum(vector<int> scores) {
+  int sum = 0;
+  for(int i=0;i<scores.size();i++){
+    sum += scores.at(i);
+  }
+  return sum;
+}
 
-    for (int i = 0; i < vec.size(); i++) {
-        cout << vec.at(i) << endl;
-    }
+
+void output(int sum_a, int sum_b, int sum_c) {
+  cout << sum_a * sum_b * sum_c << endl;
+}
+
+
+vector<int> input(int N) {
+  vector<int> vec(N);
+  for (int i = 0; i < N; i++) {
+    cin >> vec.at(i);
+  }
+  return vec;
+}
+
+int main() {
+  int N;
+  cin >> N;
+
+  vector<int> A = input(N);
+  vector<int> B = input(N);
+  vector<int> C = input(N);
+
+  int sum_A = sum(A);
+  int sum_B = sum(B);
+  int sum_C = sum(C);
+
+  output(sum_A, sum_B, sum_C);
 }
