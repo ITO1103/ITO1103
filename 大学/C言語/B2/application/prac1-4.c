06471f9a7777725c175a16042c038f1aa252d824
ITO110 <t22j209ai@ous.jp>
2023-09-28 22:48:58 +0900

    update

--- 大学/C言語/B2/application/prac1-4.c
+++ 大学/C言語/B2/application/prac1-4.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
 int main(void){
-    int dim[][] = {{85, 90, 75}, {90, 85, 80}};
+    int dim[][3] = {{85, 90, 75}, {90, 85, 80}};
+    //        ↑なんでここだけ入れる必要あるん？
 
-    for(int i=0;i<3;i++){
+    for(int i=0;i<2;i++){
         for(int j=0;j<3;j++){
-            printf("a[%d][%d] = %d",i,j,dim[i][j]);
+            printf("a[%d][%d] = %d\n",i,j,dim[i][j]);
         }
     }
 }
