86c8f3b67bf8345f12dcbac587de630308a1eb3e
ITO110 <t22j209ai@ous.jp>
2023-10-20 17:45:43 +0900

    update

--- AtCoder/APG4b[C++]/EX18.cpp
+++ AtCoder/APG4b[C++]/EX18.cpp
@@ -2,31 +2,13 @@
 using namespace std;
 
 int main() {
-
-  // int型の2次元配列(3×4要素の)の宣言
-  vector<vector<int>> data(3, vector<int>(4));
-
-  // 入力 (2重ループを用いる)
-  for (int i = 0; i < 3; i++) {
-    for (int j = 0; j < 4; j++) {
-      cin >> data.at(i).at(j);
-    }
+  int N, M;
+  cin >> N >> M;
+  vector<int> A(M), B(M);
+  for (int i = 0; i < M; i++) {
+    cin >> A.at(i) >> B.at(i);
   }
 
-  // 0の個数を数える
-  int count = 0;
-
-  for (int i = 0; i < 3; i++) {
-    for (int j = 0; j < 4; j++) {
-
-      // 上からi番目、左からj番目の画素が0かを判定
-      if (data.at(i).at(j) == 0) {
-        count++;
-      }
-
-    }
-  }
-
-   cout << count << endl;
-
+  // ここにプログラムを追記
+  // (ここで"試合結果の表"の2次元配列を宣言)
 }
