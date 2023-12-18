7a7e8a6eec339e0bde891e339c3558de5c5d1b7a
ITO110 <t22j209ai@ous.jp>
2023-12-19 00:07:58 +0900

    update

--- Paiza/test.cpp
+++ Paiza/test.cpp
@@ -2,8 +2,18 @@
 using namespace std;
 
 int main() {
-    int N;
-    cin >> N;
-    cout << setw(3) << setfill(' ') << N << endl;
+    for(int i=1;i<=100;i++){
+        if(i%3==0){
+            cout << "Fizz" << endl;
+        }else if(i%5==0){
+            cout << "Buzz" << endl;
+        }else if(i%3==0 && i%5==0){
+            cout << "FizzBuzz" << endl;
+        }else if(i==100){
+            cout << i;
+        }else{
+            cout << i << endl;
+        }
+    }
 
 }
